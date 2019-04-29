377fe6cede0e4ceb3542075c3d75e678d3a527d6
pjastr <24464854+pjastr@users.noreply.github.com>
2019-04-29 07:59:42 +0200

    fix

--- 6_2_2/main.c
+++ 6_2_2/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* f(int n, int m)
+int(* f(int n, int m))[]
 {
     return malloc(n*sizeof(int[m]));
 }
@@ -11,7 +11,7 @@ int main()
     printf("%d\n",sizeof(int*));
     printf("%d\n",sizeof(int));
     int n=3, m=4;  //zmienne na rozmiar
-    int *tablica; //stworzenie tablicy
+    int **tablica; //stworzenie tablicy
     tablica=f(n,m); //przypisanie do tablicy wyniku funkcji
     *tablica=0; //ustawienie wartosci w miejsce 0,0
     *(tablica+2*m)=7;  //ustawienie wartosci w miejsce 2,0
