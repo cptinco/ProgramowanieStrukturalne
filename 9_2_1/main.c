469f143c9988e98e4ed7d2a4feea332c74917368
Piotrek <24464854+pjastr@users.noreply.github.com>
2019-05-19 18:30:04 +0200

    9_2_1-2

---
+++ 9_2_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define suma(a,b,c) (a+b+c)
+
+int main()
+{
+    printf("%d", suma(5,6,7));
+    printf("%c", suma('d','e','t'));
+    return 0;
+}
---
+++ 9_2_2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define srednia(a,b,c) ((a+b+c)/3.0f)
+
+int main()
+{
+    printf("%f", srednia(5,6,8));
+    return 0;
+}
